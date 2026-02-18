281a734fd4a219394fe92010d6dde2dc0fb22cbe
이승환 <seaung13@gmail.com>
2026-02-18 13:18:31 +0900

    [ECS-12] feat: step 0 Jest 테스트 연습

--- src/step0-jest/calculator.test.ts
+++ src/step0-jest/calculator.test.ts
@@ -1,10 +1,32 @@
 // Step 0: Jest 테스트 기초
 // 이 파일에서 describe / it / expect 사용법을 연습합니다.
 
+import { add, divide, multiply, subtract } from "./calculator";
+
 describe('calculator', () => {
     // TODO: 아래 테스트를 완성하세요
+    // given
+    const a: number = 1;
+    const b: number = 2;
 
-    it('1 + 1은 2이다', () => {
-        // expect(add(1, 1)).toBe(2);
+    // when + then
+    it('덧셈을 수행한다.', () => {
+        expect(add(a, b)).toBe(3);
     });
+
+    it('뺄셈을 수행한다.', () => {
+        expect((subtract(a, b))).toBe(-1);
+    });
+
+    it('두 수를 곱한다.', () => {
+        expect((multiply(a, b))).toBe(2);
+    });
+
+    it('0이 아닌 수로 나눌 수 있다.', () => {
+        expect((divide(a, b))).toBe(0.5);
+    })
+
+    it('0으로 나눌 경우 예외가 발생한다.', () => {
+        expect(() => divide(a, 0)).toThrow('0으로 나눌 수 없습니다.');
+    })
 });
--- src/step0-jest/calculator.ts
+++ src/step0-jest/calculator.ts
@@ -2,21 +2,20 @@
 // 이 파일에 calculator 함수들을 구현하세요.
 
 export function add(a: number, b: number): number {
-    // TODO: 구현
-    return 0;
+    return a + b;
 }
 
 export function subtract(a: number, b: number): number {
-    // TODO: 구현
-    return 0;
+    return a - b;
 }
 
 export function multiply(a: number, b: number): number {
-    // TODO: 구현
-    return 0;
+    return a * b;
 }
 
 export function divide(a: number, b: number): number {
-    // TODO: 구현 (0으로 나누면 에러 던지기)
-    return 0;
+    if (b === 0) {
+        throw new Error('0으로 나눌 수 없습니다.');
+    }
+    return a / b;
 }
